2323d39ac3cedf59284cb0a2af68fb20978ef698
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-06-08 23:42:25 +0530

    Perform Validation Employee Payroll Data

--- emp_payroll.js
+++ emp_payroll.js
@@ -8,62 +8,76 @@ function EmployeePayroll(name, gender, department, salary, startDate, notes) {
   var _notes = notes;
 
   Object.defineProperty(this, "name", {
-      get: function() {
-          return _name;
-      },
-      set: function(name) {
-          var nameRegex = /^[A-Z][a-zA-Z\s]{2,}$/;
-          if (nameRegex.test(name)) {
-              _name = name;
-          } else {
-              throw new Error("Invalid name format!");
-          }
+    get: function() {
+      return _name;
+    },
+    set: function(name) {
+      var nameRegex = /^[A-Z][a-zA-Z\s]{2,}$/;
+      if (nameRegex.test(name)) {
+        _name = name;
+      } else {
+        throw new Error("Invalid name format!");
       }
+    }
   });
 
   Object.defineProperty(this, "gender", {
-      get: function() {
-          return _gender;
-      },
-      set: function(gender) {
-          _gender = gender;
-      }
+    get: function() {
+      return _gender;
+    },
+    set: function(gender) {
+      _gender = gender;
+    }
   });
 
   Object.defineProperty(this, "department", {
-      get: function() {
-          return _department;
-      },
-      set: function(department) {
-          _department = department;
-      }
+    get: function() {
+      return _department;
+    },
+    set: function(department) {
+      _department = department;
+    }
   });
 
   Object.defineProperty(this, "salary", {
-      get: function() {
-          return _salary;
-      },
-      set: function(salary) {
-          if (salary > 0) {
-              _salary = salary;
-          } else {
-              throw new Error("Salary must be a positive number!");
-          }
+    get: function() {
+      return _salary;
+    },
+    set: function(salary) {
+      if (salary > 0) {
+        _salary = salary;
+      } else {
+        throw new Error("Salary must be a positive number!");
       }
+    }
   });
 
   Object.defineProperty(this, "startDate", {
-      get: function() {
-          return _startDate;
-      },
-      set: function(startDate) {
-          _startDate = startDate;
+    get: function() {
+      return _startDate;
+    },
+    set: function(startDate) {
+      var currentDate = new Date();
+      var joiningDate = new Date(startDate);
+      var thirtyDaysAgo = new Date();
+      thirtyDaysAgo.setDate(currentDate.getDate() - 30);
+
+      if (joiningDate > currentDate) {
+        throw new Error("Start date cannot be a future date!");
+      } else if (joiningDate < thirtyDaysAgo) {
+        throw new Error("Start date should be within 30 days of joining!");
+      } else {
+        _startDate = startDate;
       }
+    }
   });
 
   Object.defineProperty(this, "notes", {
-      get: function() {
-          return _notes;
-      }
+    get: function() {
+      return _notes;
+    },
+    set: function(notes) {
+      _notes = notes;
+    }
   });
 }
