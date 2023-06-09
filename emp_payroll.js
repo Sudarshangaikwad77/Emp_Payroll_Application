8576bbece5e2cc5217da2a5c834186c3d6e4a738
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-06-09 23:29:17 +0530

    added refactor code to UC-10

--- emp_payroll.js
+++ emp_payroll.js
@@ -1,83 +1,86 @@
 
-function EmployeePayroll(name, gender, department, salary, startDate, notes) {
-  var _name = name;
-  var _gender = gender;
-  var _department = department;
-  var _salary = salary;
-  var _startDate = startDate;
-  var _notes = notes;
+class EmployeePayroll {
+  constructor() {
+    this.name = "";
+    this.profile = "";
+    this.gender = "";
+    this.department = [];
+    this.salary = 0;
+    this.notes = "";
+  }
+
+  setName(name) {
+    if (name && name.trim().length >= 3 && /^[A-Z][A-Za-z\s]*$/.test(name)) {
+      this.name = name.trim();
+    } else {
+      throw new Error("Invalid name! Name must start with a capital letter and have a minimum of 3 characters.");
+    }
+  }
 
   Object.defineProperty(this, "name", {
-    get: function() {
-      return _name;
-    },
-    set: function(name) {
-      var nameRegex = /^[A-Z][a-zA-Z\s]{2,}$/;
-      if (nameRegex.test(name)) {
-        _name = name;
-      } else {
-        throw new Error("Invalid name format!");
+      get: function() {
+          return _name;
+      },
+      set: function(name) {
+          var nameRegex = /^[A-Z][a-zA-Z\s]{2,}$/;
+          if (nameRegex.test(name)) {
+              _name = name;
+          } else {
+              throw new Error("Invalid name format!");
+          }
       }
-    }
   });
 
   Object.defineProperty(this, "gender", {
-    get: function() {
-      return _gender;
-    },
-    set: function(gender) {
-      _gender = gender;
-    }
+      get: function() {
+          return _gender;
+      },
+      set: function(gender) {
+          _gender = gender;
+      }
   });
 
   Object.defineProperty(this, "department", {
-    get: function() {
-      return _department;
-    },
-    set: function(department) {
-      _department = department;
-    }
+      get: function() {
+          return _department;
+      },
+      set: function(department) {
+          _department = department;
+      }
   });
 
   Object.defineProperty(this, "salary", {
-    get: function() {
-      return _salary;
-    },
-    set: function(salary) {
-      if (salary > 0) {
-        _salary = salary;
-      } else {
-        throw new Error("Salary must be a positive number!");
+      get: function() {
+          return _salary;
+      },
+      set: function(salary) {
+          if (salary > 0) {
+              _salary = salary;
+          } else {
+              throw new Error("Salary must be a positive number!");
+          }
       }
-    }
   });
 
   Object.defineProperty(this, "startDate", {
-    get: function() {
-      return _startDate;
-    },
-    set: function(startDate) {
-      var currentDate = new Date();
-      var joiningDate = new Date(startDate);
-      var thirtyDaysAgo = new Date();
-      thirtyDaysAgo.setDate(currentDate.getDate() - 30);
-
-      if (joiningDate > currentDate) {
-        throw new Error("Start date cannot be a future date!");
-      } else if (joiningDate < thirtyDaysAgo) {
-        throw new Error("Start date should be within 30 days of joining!");
-      } else {
-        _startDate = startDate;
+      get: function() {
+          return _startDate;
+      },
+      set: function(startDate) {
+          _startDate = startDate;
       }
-    }
   });
 
   Object.defineProperty(this, "notes", {
-    get: function() {
-      return _notes;
-    },
-    set: function(notes) {
-      _notes = notes;
-    }
+      get: function() {
+          return _notes;
+      }
   });
 }
+
+const salaryRange = document.getElementById('salary');
+const salaryOutput = document.querySelector('.salary-output');
+
+salaryRange.addEventListener('input', function() {
+  salaryOutput.textContent = salaryRange.value;
+});
