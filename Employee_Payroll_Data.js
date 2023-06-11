79e6387ddc6c9a4f567f916417af0b1009249357
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-06-11 23:07:25 +0530

    Ability to set Event Listeners when Document is loaded

--- Employee_Payroll_Data.js
+++ Employee_Payroll_Data.js
@@ -1,4 +1,3 @@
-
 class EmployeePayrollData {
   constructor(name, profile, gender, departments, salary, startDate, notes) {
     this._name = name;
@@ -91,3 +90,29 @@ class EmployeePayrollData {
        ",salary=" + this.salary +", startDate=" + empDate + ", note="+ this.note; 
     }
   }
+
+  window. addEventListener( 'DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name') ;
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+    if (name. value. length == 0) {
+    textError.textContent = "";
+    return;
+    }
+    try {
+    (new EmployeePayrollData()).name = name. value; ;
+    textError.textContent = "";
+    } catch (e) {
+    textError.textContent = e;
+    }
+  });
+  const salary = document.querySelector('#salary');
+  const output = document. querySelector('.salary-output');
+  output. textContent = salary.value;
+  salary. addEventListener('input', function() {
+  output.textContent = salary.value;
+});
+  });
+    
+     
+    
