d2339d53ff3fefb053ea79c3784902aa893cb41b
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-06-12 23:38:56 +0530

    Added code to displY emp details

--- Emp.js
+++ Emp.js
@@ -1,19 +1,65 @@
-function populateEmployeeData() {
-  let name = document.getElementById("name").value;
-  let profile = document.querySelector("input[name='profile']:checked").value;
-  let gender = document.querySelector("input[name='gender']:checked").value;
-  let departments = [];
-  let departmentCheckboxes = document.querySelectorAll("input[name='department']:checked");
-  departmentCheckboxes.forEach(function(checkbox) {
-    departments.push(checkbox.value);
-  });
-  let salary = document.getElementById("salary").value;
-  let startDate = document.getElementById("day").value + "-" + document.getElementById("month").value + "-" + document.getElementById("year").value;
-  let notes = document.getElementById("notes").value;
-  let employeeData = new EmployeePayrollData(name, profile, gender, departments, salary, startDate, notes);
-  if (employeeData.validateData()) {
-    console.log("Employee Data:", employeeData);
-  } else {
-    console.log("Validation Error: Invalid employee data");
-  }
-}  
+window. addEventListener('DOMContentLoaded', (event) => {
+  createInnerHtml();
+});
+const createInnerHtml = () => {
+ const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" + "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+ let innerHtml = `${headerHtml}`;
+ let empPayrollList = creteEmployeePayrollJSON();
+ for (const empPayrollData of empPayrollList) {
+  innerHtml = `${innerHtml}
+ <tr>
+      <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+      <td>${empPayrollData._name}</td>
+      <td>${empPayrollData._gender}</td>
+      <td>${getDeptHtml(empPayrollData._department)}</td>
+      <td>${empPayrollData._salary}</td>
+      <td>${empPayrollData._startDate}</td>
+      <td>
+          <img id="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="delete-black-18dp.svg">
+          <img id="${empPayrollData._id}" alt="edit" onclick="update(this)" src="create-black-18dp.svg">
+      </td>
+ </tr>
+ `;
+ }
+ document.querySelector("#table-display").innerHTML = innerHtml;
+ }
+
+ const getDeptHtml = (deptList) => {
+     let deptHtml = '';
+     for (const dept of deptList) {
+         deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+     }
+     return deptHtml;
+ }
+
+ const creteEmployeePayrollJSON = () => {
+     let empPayrollListLocal = [
+     {
+     _name: 'Kanishk Singhal',
+     _gender: 'male',
+     _department: [
+     'Engineering',
+     'Finance'
+     ],
+     _salary: '500000',
+     _startDate: '29 Oct 2022',
+     _note: '',
+     _id: new Date().getTime(),
+     _ProfilePic: 'Ellipse -2.png'
+ },
+ {
+     _name: 'Harsh Agrawal',
+     _gender: 'male',
+     _department: [
+         'Engineering',
+         'Finance'
+     ],
+     _salary: '800000',
+     _startDate: '06 Mar 2022',
+     _note: '',
+     _id: new Date().getTime() + 1,
+     _ProfilePic: 'Ellipse -9.png'
+ }
+     ];
+     return empPayrollListLocal;
+ }
