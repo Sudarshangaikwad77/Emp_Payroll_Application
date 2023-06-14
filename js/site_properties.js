510ff624c6c1ef5d9de1ae7f2e5e850af962050b
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-06-14 13:59:36 +0530

    TestRestServicesUsingPrommise

---
+++ js/site_properties.js
@@ -0,0 +1,4 @@
+let site_properties = {
+    home_page: "../Pages/home.html",
+    add_emp_payroll_page: "../Pages/Payroll_form.html"
+};
--- site_properties.js
+++
@@ -1,5 +0,0 @@
-const site_properties = {
-    use_local_storage: "true",
-    add_employee_page: "../pages/Index.html",
-    home_page: "../pages/home.html"
-};
